5e57affa87932585b9730b5b65a7cb602bf0bddb
Rhea Roy <rroy.rhea@gmail.com>
2019-03-19 11:03:27 -0700

    can loop through json and now adds yes buttons to array

--- app.py
+++ app.py
@@ -5,16 +5,21 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 
+from forms import ReviewForm
+
 app = Flask(__name__)
 CORS(app)
 
 DEBUG = True
 PORT = 8000
 
+app.secret_key = 'adkjfalj.adflja.dfnasdf.asd'
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def swipePage(swipe=None):
+    return render_template('swipe.html',swipe=swipe)
+
+
 
 if __name__ == '__main__':
     app.run(debug=DEBUG, port=PORT)
--- forms.py
+++ forms.py
@@ -1,2 +1,7 @@
 from flask_wtf import FlaskForm as Form
 from wtforms import TextField, TextAreaField, SubmitField
+
+
+class ReviewForm(Form):
+  review = TextAreaField("Review this plant: ")
+  submit = SubmitField('Submit Review')
