57847e9d7a71395b8226b450bebf74c472fbea13
teknoformula <seraficaj@gmail.com>
2019-03-21 14:10:06 -0700

    merged jason with master

--- static/app.js
+++ static/app.js
@@ -1,6 +1,5 @@
 $( document ).ready(function() {
     $.getJSON(("static/plant.json"), function(json) {
-
         function setHtml(z){
             $(".species").html('Species: '+json[z].species)
             $(".difficulty").html('Level of Difficulty: '+json[z].Difficulty)
@@ -8,28 +7,27 @@ $( document ).ready(function() {
             $('.plantImage').attr('src', json[z].image)
             $('.light').html('Amount of Light: ' + json[z].light)
         }
-    
         //initial plant showing
         setHtml(0)
 
         var i = 0;  
 
 
-        $(".yesButton").click(function(){
-            i= (i+1)%json.length;
-            setHtml(i)
-            if (i==0){
-                $('.yasPlants').append('<li>' + json[json.length-1].species+ '</li>')
-            }else{
-                $('.yasPlants').append('<li>' + json[i-1].species+ '</li>')
-            }
+        // $(".yesButton").click(function(){
+        //     i= (i+1)%json.length;
+        //     setHtml(i)
+        //     if (i==0){
+        //         $('.yasPlants').append('<li>' + json[json.length-1].species+ '</li>')
+        //     }else{
+        //         $('.yasPlants').append('<li>' + json[i-1].species+ '</li>')
+        //     }
 
-        }); //end of yes click function 
+        // }); //end of yes click function 
 
-        $(".noButton").click(function(){
-            i= (i+1)%json.length;
-            setHtml(i)
-        }); //end of no click function
+        // $(".noButton").click(function(){
+        //     i= (i+1)%json.length;
+        //     setHtml(i)
+        // }); //end of no click function
     });//end of getJson functuion
 
 }); //end of document.ready  
