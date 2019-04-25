503c37e8d0062155899eeaf8d414aae9fdeb94b5
Rhea Roy <rroy.rhea@gmail.com>
2019-04-24 18:29:47 -0700

    another1

--- app.py
+++ app.py
@@ -180,20 +180,22 @@ def logout():
     flash("Logged Out")
     return redirect(url_for('loginPage'))
 
-
+if 'ON_HEROKU' in os.environ:
+    print('hitting ')
+    models.initialize()
 
 if __name__ == '__main__':
     models.initialize()
-    # try:
-    #     models.User.create_user(
-    #         username='rroy',
-    #         email="rhea@rhea.com",
-    #         password='password',
-    #         city= 'San Jose',
-    #         admin=True
-    #     )
-    # except ValueError:
-    #     pass
+    try:
+        models.User.create_user(
+            username='rroy',
+            email="rhea@rhea.com",
+            password='password',
+            city= 'San Jose',
+            admin=True
+        )
+    except ValueError:
+        pass
 
 
     app.run(debug=DEBUG, port=PORT)
